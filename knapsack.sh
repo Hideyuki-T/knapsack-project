f0861778c81636df3c15c192107383a8ee21c159
Hideyuki-T <hide27yuki@gmail.com>
2025-04-08 17:12:56 +0900

    [feat]最大価値を求める処理を追加

---
+++ knapsack.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source ./data.sh
+
+for ((w=0; w<=capacity; w++)); do
+  dp[$w]=0
+done
+
+for ((i=0; i<item_count; i++)); do
+  for ((w=capacity; w>=weights[i];w--)); do
+    candidate=$(( dp[w - weights[i]] + values[i] ))
+    if (( candidate > dp[w] )); then
+      dp [$w]=$candidate
+    fi
+  done
+done
+
+echo "最大価値： ${dp[$capacity]}"
