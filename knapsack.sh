e8af7e2d256ef2a57ed0ae47db3ff50f1264b600
Hideyuki-T <hide27yuki@gmail.com>
2025-04-08 17:43:53 +0900

    [fix]

--- knapsack.sh
+++ knapsack.sh
@@ -6,14 +6,12 @@ export LC_ALL=en_US.UTF-8
 source ./data.sh
 
 # dp は各重さに対応する最大価値
-# sel は各重さに対応する、選択した品物のインデックス（スペース区切りの文字列）を記録
 declare -a dp
 declare -a sel
 
-# 初期化：重さ 0 から capacity まで
 for ((w=0; w<=capacity; w++)); do
   dp[$w]=0
-  sel[$w]=""  # 空文字
+  sel[$w]=""
 done
 
 # DP の計算と同時に、選択情報も更新する
