6fcf770f5577f3766e842cc3ef43390373cf912b
Hideyuki-T <hide27yuki@gmail.com>
2025-04-08 17:21:51 +0900

    [fix]knapsack.sh | data.sh

--- knapsack.sh
+++ knapsack.sh
@@ -7,12 +7,12 @@ for ((w=0; w<=capacity; w++)); do
 done
 
 for ((i=0; i<item_count; i++)); do
-  for ((w=capacity; w>=weights[i];w--)); do
+  for ((w=capacity; w>=weights[i]; w--)); do
     candidate=$(( dp[w - weights[i]] + values[i] ))
     if (( candidate > dp[w] )); then
-      dp [$w]=$candidate
+      dp[$w]=$candidate
     fi
   done
 done
 
-echo "最大価値： ${dp[$capacity]}"
+echo "最大価値: ${dp[$capacity]}"
